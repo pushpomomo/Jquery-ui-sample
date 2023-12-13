1d0a01526c736579dd38b82c2b0504b5d66d8d97
pushpo Momo <146898831+pushpomomo@users.noreply.github.com>
2023-12-13 12:15:55 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,150 @@
+// text chang
+
+function textChange(){
+	document.getElementById('t_h3').innerHTML="Hello";
+}
+
+
+
+// show date
+function showDate(){
+       document.getElementById('sd').innerHTML=Date();
+}
+
+
+
+// bulb on/off
+function bulbOn(){
+       document.getElementById('img').src="images/bulbon.gif";
+}
+function bulbOff(){
+       document.getElementById('img').src="images/bulboff.gif";
+}
+
+// text change
+function textChange2(){
+	   document.getElementById('tx_h3').innerHTML="Awesome";
+
+}
+
+// // text change mouseover
+function textChange3(){
+	document.getElementById('m_o').innerHTML="Hello world";
+}
+
+
+// // text change mouse out
+function textChange4(){
+	document.getElementById('m_out').innerHTML="Hello world";
+}
+
+
+// // text change mouse enter
+function textChange5(){
+	document.getElementById('m_enter').innerHTML="Hello";
+}
+
+
+// // show boder
+// function showBorder(){
+// 	document.getElementById('sb').style.border="3px solid blue";
+// }
+
+// // show hide text
+// function showText(){
+// 	document.getElementById('t_sh').style.display="block";
+// }
+// function hideText(){
+// 	document.getElementById('t_sh').style.display="none";
+// }
+
+// // show result
+
+// var number1 = 7;
+// var number2 = 3;
+
+// var result = number1 + number2;
+
+// function showResult(){
+// 	document.getElementById('result').innerHTML=result;
+// }
+
+// // display result
+
+// let number1, number2; 
+// number1 = 19;
+// number2 = 23;
+// number1 += number2;
+
+// function showResult(){
+// 	document.getElementById('amount').innerHTML=number1;
+// }
+
+// // imoji
+// function smiley(){
+//        document.getElementById('imj')src="images/smiley.gif";
+// }
+
+// function crying(){
+//        document.getElementById('imj')src="images/crying.gif";
+// }
+
+
+// // class name
+// function cName(){
+// 	document.getElementsByClassName('p')[2].innerHTML="world";
+// }
+
+
+// tag name
+// function cName(){
+// 	let data = document.getElementsByTagName('h1');
+// 	alert(data.length);
+// }
+
+
+// function fontSize(){
+// 	document.getElementsByClassName('dc').style.font="Size 35px";
+// }
+
+// function(){
+// 	document.getElementById(demo).innerHTML="hello Mahi";
+// }
+
+
+//display none 
+// function displayNone(){
+// 	document.getElementById('d_n').style.display="none";
+// }
+
+//display block
+// function displayBlock(){
+// 	document.getElementById('d_block').style.display="block";
+// }
+
+
+// //display result
+// let x, y, z;
+// x = 7;
+// y = 5;
+// z = x - y;
+
+// function showResult(){
+// 	document.getElementById('operator').innerHTML= z;
+// }
+
+
+// //display string
+
+// let x = 16 + 'Volvo';
+
+// function showValue(){
+// 	document.getElementById('string').innerHTML= z;
+// }
+// // function
+// let result = myFunction(5, 8);
+// function myFunction(p1, p2){
+//        return p1 * p2;
+//        document.getElementById('m_f').innerHTML= result;
+// }
+
